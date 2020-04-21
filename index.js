f3933bd127aa076d4736ee12292705feff2b99a4
nathanieltjoa <62280439+nathanieltjoa@users.noreply.github.com>
2020-04-21 14:56:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.delete('/api/orang/:id',(req,res)=>{
     res.send(orang);
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>console.log("Jalan "+port));
